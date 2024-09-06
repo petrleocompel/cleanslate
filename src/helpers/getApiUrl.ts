1f3f5af542d721dacb5b56683fa48a89dc74d35a
Petr Leo Compel <petrleocompel@gmail.com>
2024-09-06 22:40:18 +0200

    fix: ts error of getApiUrl with window

--- src/helpers/getApiUrl.ts
+++ src/helpers/getApiUrl.ts
@@ -1,9 +1,15 @@
-import { isProduction } from './isProduction'
+import { isProduction } from "./isProduction";
 
 export const getApiUrl = () => {
   if (isProduction()) {
-    // @ts-ignore window not defined
-    return [`https://${window.location.hostname}/v1/graphql`, `wss://${window.location.hostname}/v1/graphql`]
+    if (typeof window === "undefined")
+      return ["https://localhost/v1/graphql", "wss://localhost/v1/graphql"];
+    return [
+      // @ts-ignore - window.navigator.standalone only exists on iOS
+      `https://${window.location.hostname}/v1/graphql`,
+      // @ts-ignore - window.navigator.standalone only exists on iOS
+      `wss://${window.location.hostname}/v1/graphql`,
+    ];
   }
-  return ['https://localhost/v1/graphql', 'wss://localhost/v1/graphql']
-}
+  return ["https://localhost/v1/graphql", "wss://localhost/v1/graphql"];
+};
