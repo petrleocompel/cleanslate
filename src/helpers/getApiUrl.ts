687a02bc2c772585b572c61c86374865eaa0c292
Petr Leo Compel <petrleocompel@gmail.com>
2024-09-06 20:30:26 +0000

    fix: ts ignore for window url

--- src/helpers/getApiUrl.ts
+++ src/helpers/getApiUrl.ts
@@ -2,6 +2,7 @@ import { isProduction } from './isProduction'
 
 export const getApiUrl = () => {
   if (isProduction()) {
+    // @ts-ignore window not defined
     return [`https://${window.location.hostname}/v1/graphql`, `wss://${window.location.hostname}/v1/graphql`]
   }
   return ['https://localhost/v1/graphql', 'wss://localhost/v1/graphql']
